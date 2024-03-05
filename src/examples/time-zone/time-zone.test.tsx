2e9aa4a71334b17683d4abd966e873224c701973
Bence Fekete <fekete965@gmail.com>
2024-03-05 21:32:05 +0000

    Finish time-zones.test.tsx

--- src/examples/time-zone/time-zone.test.tsx
+++ src/examples/time-zone/time-zone.test.tsx
@@ -1,12 +1,21 @@
-import { test, expect, vi } from 'vitest';
+import { afterEach, beforeEach, test, expect, vi } from 'vitest';
 import { render } from 'test/utilities';
 import TimeZone from '.';
+
+beforeEach(() => {
+  vi.useFakeTimers();
+  vi.setSystemTime(1709668437094);
+});
+
+afterEach(() => {
+  vi.useRealTimers();
+});
 
 test('it should render successfully', () => {
   render(<TimeZone />);
 });
 
-test.fails('should match the snapshot', async () => {
+test('should match the snapshot', async () => {
   const { container } = render(<TimeZone />);
   expect(container).toMatchSnapshot();
 });
